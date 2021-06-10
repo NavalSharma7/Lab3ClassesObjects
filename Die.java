3975c61a470074951f3cb841a40049300420006b
NavalSharma7 <A00241484@myCambrian.ca>
2021-06-11 03:16:04 +0530

    Update Die.java
    program description update

--- Die.java
+++ Die.java
@@ -2,7 +2,9 @@
     Naval Sharma
     A00241484
 
-		This program has certain array tools like -
+		This program has a Die class which is used to create any n-sieded die,
+		It includes properties that depic it's features and what side is up..
+		The roll method inacts a real life roll of the die and gives out a random side..
 
 */
 
@@ -31,11 +33,13 @@ public class Die
 
 	//constructor with one argument sets the numberOfSides and the type accroding
 	//to that and givees a ramdom value to the currentSideUp
+
 	public Die(int numberOfSides){
 		this.numberOfSides = numberOfSides;
 		this.type = "d" + numberOfSides;
 		this.currentSideUp = (int) (Math.random() * (numberOfSides - 1)) + 1;
 	}
+
 	//constructor with two argument sets the numberOfSides and the type
 	//to that and gives a ramdom value to the currentSideUp..
 
@@ -45,6 +49,8 @@ public class Die
 		this.currentSideUp = (int) (Math.random() * (numberOfSides - 1)) + 1;
 	}
 
+
+	//accessors and mutators methods for type, numberOfSides,currentSideUp..
 	public String getType(){
 		return type;
 	}
@@ -60,15 +66,20 @@ public class Die
 		this.type = type;
 	}
 
+	// set the type according as we set the number of sides
 	public void setNumOfSides(int numberOfSides){
 		this.numberOfSides = numberOfSides;
 		this.type = "d" + numberOfSides;
 	}
 
+
 	public void setCurrentSideUp(int currentSideUp){
 		this.currentSideUp = currentSideUp;
 	}
 
+
+	// the roll method which generrates a ramdom value between 1 and numberOfSides
+	//and assigns that as the cuurentSideup..
 	public void roll(){
 		setCurrentSideUp((int) (Math.random() * (numberOfSides - 1)) + 1);
 	}
